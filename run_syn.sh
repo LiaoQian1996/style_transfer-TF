3b836b05dcd6670faf7073be03c9e78437d5db36
LiaoQian1996 <cheer-me@outlook.com>
2019-10-07 12:24:53 +0800

    L-BFG-S instead of Adam

--- run_syn.sh
+++ run_syn.sh
@@ -2,9 +2,9 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
     --task_mode texture_synthesis \
     --texture_shape -1 -1 \
     --output_dir ./results/ \
-    --target_dir ./imgs/cobble.png \
+    --target_dir ./imgs/tomato.png \
     --initials noise \
     --top_style_layer VGG54 \
     --max_iter 500 \
-    --W_tv 0.001
-    #--vgg_ckpt ./vgg19/vgg_19.ckpt
+    --W_tv 0.001 \
+    --vgg_ckpt ./vgg19/vgg_19.ckpt
--- run_transfer.sh
+++ run_transfer.sh
@@ -2,9 +2,9 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
     --task_mode style_transfer \
     --output_dir ./results/ \
     --target_dir ./imgs/starry-night.png \
-    --content_dir ./imgs/road.png \
+    --content_dir ./imgs/tubingen.png \
     --top_style_layer VGG54 \
     --max_iter 50 \
     --W_tv 0.001 \
     --W_content 1e-6 \
-    #--vgg_ckpt ./vgg19/vgg_19.ckpt
+    --vgg_ckpt ./vgg19/vgg_19.ckpt
